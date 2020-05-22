1d3f498c93be0c563bb11786ff90ad66b390fb41
Rodwan Barbier <rodwan.barbier@absa.africa>
2020-05-22 15:03:57 +0200

    Initial Commit

---
+++ WorkoutTimer/WorkoutTimer.h
@@ -0,0 +1,18 @@
+//
+//  WorkoutTimer.h
+//  WorkoutTimer
+//
+//  Created by Rodwan on 2020/05/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WorkoutTimer.
+FOUNDATION_EXPORT double WorkoutTimerVersionNumber;
+
+//! Project version string for WorkoutTimer.
+FOUNDATION_EXPORT const unsigned char WorkoutTimerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WorkoutTimer/PublicHeader.h>
+
+
